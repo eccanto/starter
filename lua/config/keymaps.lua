521715518bf2d09811e306a049a87f2fe649d465
eccanto <ccanto.erik@gmail.com>
2023-05-27 22:45:19 -0300

    feat: add coc-rename to mapkeys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@ vim.keymap.del("n", "<leader>e")
 
 -- Editor
 vim.keymap.set({ "n", "v" }, "<leader>ec", ":Commentary<CR>", { noremap = true, desc = "Comment lines" })
-vim.keymap.set("n", "<leader>er", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, desc = "Rename variable" })
+vim.keymap.set("n", "<leader>er", "<Plug>(coc-rename)", { noremap = true, silent = true, desc = "Rename variable" })
 vim.keymap.set({ "n", "v" }, "<C-Right>", "e", { noremap = true, desc = "Move to the last letter of the word" })
 vim.keymap.set({ "n", "v" }, "<C-Left>", "b", { noremap = true, desc = "Move to the last letter of the word" })
 
