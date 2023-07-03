ed04127d5c3c71d3b1d85572a9cd490ce8e56086
eccanto <ccanto.erik@gmail.com>
2023-07-03 13:32:05 -0300

    feat: add gitsigns plugin

---
+++ lua/plugins/git.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+      signs = {
+        add = { text = '▎' },
+        change = { text = '▎' },
+        delete = { text = '' },
+        topdelete = { text = '' },
+        changedelete = { text = '▎' },
+        untracked = { text = '▎' },
+      },
+      signcolumn = true,
+      numhl = false,
+      linehl = false,
+      word_diff = false,
+      watch_gitdir = {
+        follow_files = true
+      },
+      attach_to_untracked = true,
+      current_line_blame = true,
+    },
+  },
+}
