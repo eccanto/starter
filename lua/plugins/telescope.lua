342ce0280ab0f14da217f4ac11595c9ddfef701c
Erik Ccanto <ccanto.erik@gmail.com>
2023-11-16 13:29:33 -0300

    refactor: disable lazyvim auto update and configure telescope rip_grep arguments

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,9 +9,27 @@ return {
       { "<leader>sg", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
       { "<leader>sG", Util.telescope("live_grep"), desc = "Grep (root dir)" },
       { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-      { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
+      { "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (root dir)" },
       { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent (root dir)" },
       { "<leader>fr", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
+    },
+    opts = {
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--glob",
+          "!**/.git/",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--ignore-file",
+          ".gitignore",
+        },
+      },
     },
   },
 }
