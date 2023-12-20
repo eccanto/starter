187a667119d2c7f2ce5d2eb696c47ddd9f789ba7
Erik Ccanto <ccanto.erik@gmail.com>
2023-12-20 19:00:26 -0300

    refactor: update telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,16 +4,16 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         keys = {
-            { '<leader>/', Util.telescope('live_grep', { cwd = false }), desc = 'Grep (cwd)' },
+            { '<leader>/', Util.telescope('live_grep'), desc = 'Grep (cwd)' },
             { '<leader>r', Util.telescope('resume'), desc = 'Resume' },
-            { '<leader>sg', Util.telescope('live_grep', { cwd = false }), desc = 'Grep (root dir)' },
-            { '<leader>sG', Util.telescope('live_grep', { cwd = vim.loop.cwd() }), desc = 'Grep (cwd)' },
-            { '<leader>sf', Util.telescope('files', { cwd = false }), desc = 'Find Files (root dir)' },
-            { '<leader>sF', Util.telescope('files', { cwd = vim.loop.cwd() }), desc = 'Find Files (cwd)' },
-            { '<leader>so', Util.telescope('oldfiles', { cwd = false }), desc = 'Recent (root dir)' },
-            { '<leader>sO', Util.telescope('oldfiles', { cwd = vim.loop.cwd() }), desc = 'Recent (cwd)' },
-            { '<leader>sb', Util.telescope('buffers', { cwd = false }), desc = 'Buffers (root dir)' },
-            { '<leader>sB', Util.telescope('buffers', { cwd = vim.loop.cwd() }), desc = 'Buffers (cwd)' },
+            { '<leader>sg', Util.telescope('live_grep'), desc = 'Grep (root dir)' },
+            { '<leader>sG', Util.telescope('live_grep', { cwd = false }), desc = 'Grep (cwd)' },
+            { '<leader>sf', Util.telescope('files'), desc = 'Find Files (root dir)' },
+            { '<leader>sF', Util.telescope('files', { cwd = false }), desc = 'Find Files (cwd)' },
+            { '<leader>so', Util.telescope('oldfiles'), desc = 'Recent (root dir)' },
+            { '<leader>sO', Util.telescope('oldfiles', { cwd = false }), desc = 'Recent (cwd)' },
+            { '<leader>sb', Util.telescope('buffers'), desc = 'Buffers (root dir)' },
+            { '<leader>sB', Util.telescope('buffers', { cwd = false }), desc = 'Buffers (cwd)' },
         },
         opts = {
             defaults = {
