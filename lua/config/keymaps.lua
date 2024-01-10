f2a72390b98958a3e5ebde3ee06f2f0cbc982514
Erik Ccanto <ccanto.erik@gmail.com>
2024-01-10 13:39:03 -0300

    fix: resolve keymap conflict between codeium and multiple selection plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,7 +38,7 @@ vim.keymap.set('n', '<C-A-S-Down>', '<cmd> resize -5 <CR>', { noremap = true, de
 vim.keymap.set('n', '<C-A-S-Up>', '<cmd> resize -5 <CR>', { noremap = true, desc = 'resize window to up' })
 
 -- Coc
-vim.keymap.set('i', '<CR>', 'pumvisible() ? coc#_select_confirm() : "<C-g>u<CR>"', { expr = true, silent = true })
+-- vim.keymap.set('i', '<CR>', 'pumvisible() ? coc#_select_confirm() : "<C-g>u<CR>"', { expr = true, silent = true })
 vim.keymap.set('n', '<leader>wd', '<cmd> CocDiagnostics <CR>', { noremap = true, desc = 'Open diagnistics window' })
 vim.keymap.set('n', 'gd', '<cmd> call CocActionAsync("jumpDefinition") <CR>', { noremap = true, desc = 'Go to definition' })
 vim.keymap.set('n', 'gb', '<C-o>', { noremap = true, desc = 'move to left window' })
@@ -68,4 +68,4 @@ vim.keymap.set("n", "<leader>mp", "<cmd> InstantMarkdownPreview <CR>", { noremap
 vim.keymap.set("n", "<leader>mP", "<cmd> InstantMarkdownStop <CR>", { noremap = true, desc = "Stop markdown preview" })
 
 -- Codeium
-vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+vim.keymap.set('i', '<CR>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
